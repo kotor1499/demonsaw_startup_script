2828e5dc5df1a6f910eedf0b78ac8346d577f993
tendoren <tendoren@users.noreply.github.com>
2015-09-05 06:42:23 -0700

    Update demonsaw_startup.sh

--- demonsaw_startup.sh
+++ demonsaw_startup.sh
@@ -1,5 +1,11 @@
 #!/bin/bash
 
+sudo apt-get install mesa-common-dev libcr-dev libglu1-mesa-dev -y
+
+sudo apt-get install libxcb1 libxcb1-dev libx11-xcb1 libx11-xcb-dev libxcb-keysyms1 libxcb-keysyms1-dev libxcb-image0 libxcb-image0-dev libxcb-shm0 libxcb-shm0-dev libxcb-icccm4 libxcb-icccm4-dev libxcb-sync1 libxcb-sync0-dev libxcb-render-util0 libxcb-render-util0-dev libxcb-xfixes0-dev libxrender-dev libxcb-shape0-dev libxcb-randr0-dev libxcb-glx0-dev -y
+
+ln -sf /lib/x86_64-linux-gnu/libudev.so.1 /usr/lib/x86_64-linux-gnu/libudev.so.0
+
 cd ~/demonsaw
 
 chmod +x demonsaw
